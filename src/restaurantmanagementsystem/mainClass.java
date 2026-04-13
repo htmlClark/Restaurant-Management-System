49b71048e9f525bab174292fc383f191c94ce8b5
Angelie Iranzo <iranzo.angelieg9@gmail.com>
2026-04-13 17:33:48 +0800

    Main Initial Commit  |  Restaurant Management System
    > created simple template for landing page

---
+++ src/restaurantmanagementsystem/mainClass.java
@@ -0,0 +1,7 @@
+package restaurantmanagementsystem;
+public class mainClass {
+    public static void main(String[] args) 
+    {
+        homePage HomePage = new homePage();
+        HomePage.setVisible(true);
+}}
