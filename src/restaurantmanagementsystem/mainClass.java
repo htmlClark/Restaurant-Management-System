2b68797fc8167f3e46b0a2b8a67bfe4e2bde85c8
Chick3nC0de <marcussmiguel@gmail.com>
2026-05-21 00:23:44 +0800

    OVERHAULED Waste Tracker
    Multiple Updates and Changes, over the past month. (Sorry, ngayon ko lang na commit lahat ng small changes, isang bagsak nalang ngayon 😵 )
    ADDED:
    Classes separated for Staff and Admin, with another for UI, and another to store Waste Logs.
    Elements and Layout now resemble the references in Canva.
    Working Main Functions, adding items, and special access for Admin to edit logs.
    Used Private Methods for different tabs to easily merge to other apps. (Placeholder for now).
    
    (Other Layouts, Positions of Icons, QoL, Color may differ to actual reference)

--- src/restaurantmanagementsystem/mainClass.java
+++
@@ -1,8 +0,0 @@
-package restaurantmanagementsystem;
-public class mainClass {
-    public static void main(String[] args) 
-    {
-        homePage hp = new homePage();
-        hp.setVisible(true);
-
-}}
