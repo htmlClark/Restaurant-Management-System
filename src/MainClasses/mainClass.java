a2c75274acc26c7d539569db3b1984fbe71f9cb3
Angelie Iranzo <iranzo.angelieg9@gmail.com>
2026-06-11 09:37:06 +0800

    FINAL UPDATE
    ==LOGIN CREDENTIALS==
    👩‍🍳USER
    username: EMP-001
    pass: user123
    
    👩‍💼ADMIN
    username: AD-001
    pass: admin123
    
    📄SUPER ADMIN
    username: SA-001
    pass: superadmin123

--- src/MainClasses/mainClass.java
+++ src/MainClasses/mainClass.java
@@ -5,11 +5,9 @@ import MainPlacementFrame.*;
 public class mainClass {
     public static void main(String[] args) 
     {
-        LoginPage prod = new LoginPage();
-        prod.setVisible(true);
-        
+        //checking db connection
          DB_Connection.getConnection();
         
-//        homePage HomePage = new homePage();
-//        HomePage.setVisible(true);
+        homePage HomePage = new homePage();
+        HomePage.setVisible(true);
 }}
