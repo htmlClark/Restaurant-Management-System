a65cd4c2d6dbb88591910eb32c69662eb216cd05
alexyzabc <aamariereyes@gmail.com>
2026-06-18 05:05:15 +0800

    v3 upd tax

--- src/RestaurantManagementSystem_/PaymentProcess/PaymentProcessingPage.java
+++ src/RestaurantManagementSystem_/PaymentProcess/PaymentProcessingPage.java
@@ -48,13 +48,13 @@ public class PaymentProcessingPage extends JPanel implements ActionListener {
 
     private void orderSummary()
     {
-        //Order Summary Panel
+      
         JPanel pnlOrderSummary = new JPanel(null);
         pnlOrderSummary.setBounds(0, 125, 580, 595);
         pnlOrderSummary.setBackground(colorCream);
         add(pnlOrderSummary);
 
-        //LABELS
+        
         ImageIcon paymentLogo = new ImageIcon(getClass().getResource("/src_pack/images/payment.png"));
         Image paymentSize = paymentLogo.getImage().getScaledInstance(350, 78, Image.SCALE_DEFAULT);
 
@@ -112,7 +112,6 @@ public class PaymentProcessingPage extends JPanel implements ActionListener {
         pnlPaymentMethod.setBackground(colorSalmon);
         add(pnlPaymentMethod);
 
-        // Header
         JPanel pnlMethodHeader = new JPanel(null);
         pnlMethodHeader.setBounds(0, 0, 390, 50);
         pnlMethodHeader.setBackground(colorTeal);
@@ -124,7 +123,7 @@ public class PaymentProcessingPage extends JPanel implements ActionListener {
         lblPaymentMethod.setForeground(colorWhite);
         pnlMethodHeader.add(lblPaymentMethod);
 
-        // Payment type buttons
+       
         btnCash = new JButton("CASH");
         btnCash.setBounds(20, 65, 160, 40);
         btnCash.setFont(fontBold);
@@ -143,7 +142,7 @@ public class PaymentProcessingPage extends JPanel implements ActionListener {
         btnGcash.setBackground(colorWhite);
         pnlPaymentMethod.add(btnGcash);
 
-        // Denomination buttons
+     
         btn100 = new JButton("₱100");
         btn100.setBounds(20, 185, 160, 40);
         btn100.setFont(fontBold);
@@ -174,7 +173,7 @@ public class PaymentProcessingPage extends JPanel implements ActionListener {
         btnExact.setBackground(colorWhite);
         pnlPaymentMethod.add(btnExact);
 
-        // Amount tendered
+     
         lblAmountTendered = new JLabel("AMOUNT TENDERED");
         lblAmountTendered.setBounds(20, 350, 250, 25);
         lblAmountTendered.setFont(fontBold);
@@ -202,7 +201,7 @@ public class PaymentProcessingPage extends JPanel implements ActionListener {
         lblChange.setFont(fontTotal);
         pnlPaymentMethod.add(lblChange);
 
-        // Action buttons
+   
         btnProcessPayment = new JButton("PROCESS PAYMENT");
         btnProcessPayment.setBounds(20, 500, 340, 45);
         btnProcessPayment.setFont(fontBold);
@@ -218,7 +217,7 @@ public class PaymentProcessingPage extends JPanel implements ActionListener {
         btnCancel.setBorderPainted(false);
         pnlPaymentMethod.add(btnCancel);
 
-        // Register listeners
+       
         btnCash.addActionListener(this);
         btnCard.addActionListener(this);
         btnGcash.addActionListener(this);
@@ -247,7 +246,7 @@ public class PaymentProcessingPage extends JPanel implements ActionListener {
 
     private void selectPaymentMethod(JButton selectedBtn, String paymentMethod)
     {
-        //tis is for un-selecting buttons
+        
         if (paymentMethod.equals(selectedPaymentMethod))
         {
             selectedBtn.setBackground(colorWhite);
@@ -256,7 +255,7 @@ public class PaymentProcessingPage extends JPanel implements ActionListener {
             return;
         }
 
-        //tis for unselected buttons setup
+       
         btnCard.setBackground(colorWhite);
         btnCard.setForeground(Color.BLACK);
 
@@ -266,7 +265,7 @@ public class PaymentProcessingPage extends JPanel implements ActionListener {
         btnGcash.setBackground(colorWhite);
         btnGcash.setForeground(Color.BLACK);
 
-        //tis for selected button
+    
         selectedBtn.setBackground(colorGreen);
         selectedBtn.setForeground(colorWhite);
         selectedPaymentMethod = paymentMethod;
